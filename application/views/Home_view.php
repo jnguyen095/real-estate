19953a8089ac49bda16891e13f694a1b1d91c7ba
Khang Nguyen <khang.nguyen@banvien.com>
2017-07-20 17:57:10 +0700

    Revise menu

--- application/views/Home_view.php
+++ application/views/Home_view.php
@@ -6,6 +6,7 @@
 	<title>Students Example</title>
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="<?php echo base_url()?>css/bootstrap.min.css">
+	<link rel="stylesheet" href="<?php echo base_url()?>css/mcustome.css">
 	<!-- jQuery library -->
 	<script src="<?php echo base_url()?>js/jquery.min.js"></script>
 	<!-- Latest compiled JavaScript -->
@@ -18,31 +19,7 @@
 
 <a href = "<?php echo base_url(); ?>index.php/stud/add_view">Add</a>
 
-<table border = "1">
-	<?php
-	$i = 1;
-	echo "<tr>";
-	echo "<td>Sr#</td>";
-	echo "<td>Roll No.</td>";
-	echo "<td>Name</td>";
-	echo "<td>Edit</td>";
-	echo "<td>Delete</td>";
-	echo "<tr>";
 
-	foreach($categories as $r) {
-		echo "<tr>";
-		echo "<td>".$i++."</td>";
-		echo "<td>".$r->CategoryID."</td>";
-		echo "<td>".$r->CatName."</td>";
-		echo "<td><a href = '".base_url()."stud/edit/"
-			.$r->CategoryID.".html'>Edit</a></td>";
-		echo "<td><a href = '".base_url()."stud/delete/"
-			.$r->CategoryID.".html'>Delete</a>";
-		echo "<a href=". site_url('welcome/register'); " >Register</a></td>";
-		echo "<tr>";
-	}
-	?>
-</table>
 <?php $this->load->view('/theme/footer')?>
 </div>
 </body>
--- application/views/theme/header.php
+++ application/views/theme/header.php
@@ -6,28 +6,44 @@
  * Time: 11:17 AM
  */
 ?>
-<div class="header-logo"></div>
-<nav class="navbar navbar-default m-navbar" data-spy="affix" data-offset-top="67">
-	<div class="container-fluid">
-		<ul class="nav navbar-nav">
-			<?php
-				foreach($categories as $r) {
-					if(count($child[$r->CategoryID]) > 0){
-						echo '<li role="presentation" class="dropdown">
-							<a class="dropdown-toggle" data-toggle="dropdown" href="'.base_url().'category/'.$r->CategoryID. '.html" role="button" aria-haspopup="true" aria-expanded="false">
-										'.$r->CatName.' <span class="caret"></span>
-							</a>
-							<ul class="dropdown-menu">';
-								foreach ($child[$r->CategoryID] as $k){
-									echo '<li><a href="'.base_url().'category/'.$k->CategoryID. '.html">'.$k->CatName.'</a></li>';
-								}
+<div class="header">
+	<div class="header-logo"></div>
 
-						echo '</ul></li>';
-					}else{
-						echo ' <li class="active"><a href="'.base_url().'category/'.$r->CategoryID. '.html">'.$r->CatName.'</a></li>';
+	<nav class="navbar navbar-default m-navbar" data-spy="affix" data-offset-top="67">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar4">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+			</div>
+			<a class="navbar-brand" href="<?php echo base_url() ?>"><span class="glyphicon glyphicon-home"></span></a>
+			<div id="navbar4" class="navbar-collapse collapse">
+				<ul class="nav navbar-nav">
+					<?php
+					foreach($categories as $r) {
+						if(count($child[$r->CategoryID]) > 0){
+							echo '<li role="presentation" class="dropdown">
+								<a class="dropdown-toggle" data-toggle="dropdown" href="'.base_url().'category/'.$r->CategoryID. '.html" role="button" aria-haspopup="true" aria-expanded="false">
+											'.$r->CatName.' <span class="caret"></span>
+								</a>
+								<ul class="dropdown-menu">';
+							foreach ($child[$r->CategoryID] as $k){
+								echo '<li><a href="'.base_url().'category/'.$k->CategoryID. '.html">'.$k->CatName.'</a></li>';
+							}
+
+							echo '</ul></li>';
+						}else{
+							echo ' <li class="active"><a href="'.base_url().'category/'.$r->CategoryID. '.html">'.$r->CatName.'</a></li>';
+						}
 					}
-				}
-			?>
-		</ul>
-	</div>
-</nav>
+					?>
+				</ul>
+			</div>
+			<!--/.nav-collapse -->
+		</div>
+		<!--/.container-fluid -->
+	</nav>
+</div>
