c82a7f5ed82e4be9fb38a1f266debc24412e6932
Khang Nguyen <khang.nguyen@banvien.com>
2017-10-13 15:23:48 +0700

    quick search

--- application/views/common/quick-search.php
+++ application/views/common/quick-search.php
@@ -8,18 +8,9 @@
 ?>
 <div class="quickSearch pull-right">
 	<form class="navbar-form" action="<?=base_url('/tim-kiem.html')?>" role="search">
-		<div class="input-group">
-			<div class="input-group-btn">
-				<select name="type" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-					<option value="all">Tất cả</option>
-					<option value="sale">Bđs bán</option>
-					<option value="rent">Cho thuê</option>
-				</select>
-			</div>
-			<input id="txtQuery" type="text" name="query" class="form-control" placeholder="tìm kiếm">
-			<div class="input-group-btn">
-				<button id="btnQuickSearch" class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
-			</div>
+		<div class="right-inner-addon ">
+			<i class="glyphicon glyphicon-search"></i>
+			<input type="search" name="query" class="form-control" placeholder="Tìm kiếm" />
 		</div>
 	</form>
 </div>
