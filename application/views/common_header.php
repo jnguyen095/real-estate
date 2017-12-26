00c7d7c1c52d453d90b58c7cb2e91bbe9dd3f07d
Khang Nguyen <khang.nguyen@banvien.com>
2017-12-26 21:49:45 +0700

    Previous commit

--- application/views/common_header.php
+++ application/views/common_header.php
@@ -2,7 +2,7 @@
 <link rel="icon" sizes="48x48" href="<?=base_url('/img/ico.ico')?>">
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="<?php echo base_url()?>css/bootstrap.min.css">
-<link rel="stylesheet" href="<?php echo base_url()?>css/mcustome.css">
+<link rel="stylesheet" href="<?php echo base_url()?>css/mcustome.min_v4.1.css">
 <link rel="stylesheet" href="<?php echo base_url()?>css/mobile.css">
 <!-- jQuery library -->
 <script src="<?php echo base_url()?>js/jquery.min.js"></script>
