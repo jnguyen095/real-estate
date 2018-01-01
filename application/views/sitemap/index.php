13ef2badb1760e5cd8c368718e9ad306bd198758
Khang Nguyen <khang.nguyen@banvien.com>
2018-01-01 21:35:19 +0700

    sitemap

--- application/views/sitemap/index.php
+++ application/views/sitemap/index.php
@@ -1,26 +1,18 @@
-<?php
-/**
- * Created by Khang Nguyen.
- * Email: khang.nguyen@banvien.com
- * Date: 8/28/2017
- * Time: 4:43 PM
- */
-
-?>
-
 <?php echo'<?xml version="1.0" encoding="UTF-8" ?>' ?>
 
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
 	<url>
 		<loc><?php echo base_url();?></loc>
-		<priority>1.0</priority>
+		<lastmod><?=$currentDate?></lastmod>
+		<changefreq>daily</changefreq>
+		<priority>0.9</priority>
 	</url>
-
-	<!-- Your Sitemap -->
-	<?php foreach($urlslist as $url) { ?>
+	<?php foreach($categorylist as $url) { ?>
 		<url>
 			<loc><?php echo $url;?></loc>
-			<priority>0.5</priority>
+			<lastmod>2017-12-12</lastmod>
+			<changefreq>weekly</changefreq>
+			<priority>0.3</priority>
 		</url>
 	<?php } ?>
 
